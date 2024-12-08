90eea372cdd29a1d79c566f762412dc9fd43742b
Mayuri Sontakke <86397445+mayurisontakke@users.noreply.github.com>
2024-12-08 12:38:09 +0000

    conversation of string

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,8 +1,21 @@
 let score = 24
 
-console.log(typeof score);
+//console.log(typeof score);
 
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+//****************** Operations*********************/
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+let str1 = "hello"
+let str2 = "Mayuri"
+
+let str3 = str1 + str2
+console.log(str3)
+
