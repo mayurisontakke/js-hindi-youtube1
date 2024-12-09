621c16196c7e569c1dffd56d0c3964b25c1d0843
Mayuri Sontakke <86397445+mayurisontakke@users.noreply.github.com>
2024-12-09 11:23:49 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,5 +17,33 @@ let str1 = "hello"
 let str2 = "Mayuri"
 
 let str3 = str1 + str2
-console.log(str3)
+// console.log(str3)
 
+// console.log("1"+3);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(true)
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+// console.log(gameCounter);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+
+//===
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// Stack(primitive), Heap(non-primitive)
+
+let myYoutubename = "hiteshChoudharydotcom"
