5d78d69113d39db893c563b271ef24595fbd683a
Mayuri Sontakke <86397445+mayurisontakke@users.noreply.github.com>
2024-12-13 17:00:37 +0000

    dates

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,19 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+
+// let myCreateDate = new Date(2023, 0, 23)
+// let myCreateDate = new Date(2023, 0, 23, 5, 3)
+ let myCreateDate = new Date(2023, 0, 23, 5, 3)
+console.log(myCreateDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+console.log(Date.now());
