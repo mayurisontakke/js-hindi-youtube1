3f3a6ad666d369648f6cdb2fa5f328de442cb19f
Mayuri Sontakke <86397445+mayurisontakke@users.noreply.github.com>
2024-12-15 11:30:54 +0000

    dates

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -10,10 +10,22 @@ let myDate = new Date()
 // let myCreateDate = new Date(2023, 0, 23)
 // let myCreateDate = new Date(2023, 0, 23, 5, 3)
  let myCreateDate = new Date(2023, 0, 23, 5, 3)
-console.log(myCreateDate.toLocaleString());
+// console.log(myCreateDate.toLocaleString());
 
 let myTimeStamp = Date.now()
 
-// console.log(myTimeStamp);
-// console.log(myCreateDate.getTime());
-console.log(Date.now());
+//  console.log(myTimeStamp);
+//  console.log(myCreateDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getDate());
+console.log(newDate.getMonth()+1);
+console.log(newDate.getFullYear());
+
+//
+
+newDate.toLocaleTimeString('default', {
+    weekday: "long"
+})
