b48d0097767f6a18ae15865973c87c8379d87f7c
Mayuri Sontakke <86397445+mayurisontakke@users.noreply.github.com>
2024-12-20 10:06:51 +0000

    arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -4,10 +4,28 @@
 const myArr = [0, 1, 2, 3, 4, 5]
 const myHeros = ["shaktiman", "naagraj"]
 
-const myArr2 = new array(1, 2, 3, 4)
-console.log(myArr[0]);
+const myArr2 = new Array(1, 2, 3, 4)
+console.log(myArr[1]);
 
 //  array methods
 
-myArr.push(6)
-console.log(myArr);
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.unshift()
+
+// console.log(myArr.includes(9));
+//  console.log(myArr.indexOf(19));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
