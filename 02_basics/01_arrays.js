6a3f0f923d5cc78b7babb7b02dc9aa889516e3a6
Mayuri Sontakke <86397445+mayurisontakke@users.noreply.github.com>
2024-12-20 09:06:12 +0000

    array

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,9 +1,13 @@
 
 //  arrays
 
-const myArr = [1, 2, 3, 4, 5]
+const myArr = [0, 1, 2, 3, 4, 5]
 const myHeros = ["shaktiman", "naagraj"]
 
 const myArr2 = new array(1, 2, 3, 4)
-console.log(myArr[1]);
+console.log(myArr[0]);
 
+//  array methods
+
+myArr.push(6)
+console.log(myArr);
